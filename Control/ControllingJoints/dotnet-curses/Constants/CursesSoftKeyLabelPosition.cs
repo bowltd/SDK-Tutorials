c39ca2eb57afae38a243b8275c342d0e242834f1
Daniel Camilleri <daniel@cyberselves.com>
2025-01-27 03:22:47 +0000

    Removed dotnet curses

--- Control/ControllingJoints/dotnet-curses/Constants/CursesSoftKeyLabelPosition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mindmagma.Curses
-{
-    public static class CursesSoftKeyLabelPosition
-    {
-        public static int LEFT = 0;
-        public static int CENTER = 1;
-        public static int RIGHT = 2;
-    }
-}
